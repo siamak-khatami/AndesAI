df9c8ce738b7b2df7e58a56f3737d7c97100ab9d
siamak <khatami.sia@gmail.com>
2025-02-01 22:42:57 +0100

    request and respoonse schemas updated

--- Backend/Routers/Projects/ProjectsRequestSchemas.py
+++ Backend/Routers/Projects/ProjectsRequestSchemas.py
@@ -1,39 +1,14 @@
 from pydantic import BaseModel, EmailStr, validator, Field  # this will be used to create request schemas
 from typing import Optional, List
 
-class UserLoginInfoRequest(BaseModel):
-    """
-    This class defines the schema for the api to get user information due to the login purposes.
-    """
-    email: EmailStr
-    password: str
 
-
-class UserRegistrationInfoRequest(BaseModel):
+class Project(BaseModel):
     """
-    This schema defines the input for new user creation.
+    This class defines the schema for the api to get user information due to init a project.
     """
-    email: EmailStr
+    user_id: int
     name: str
-    family: str
-    password: str
 
 
-class UserPassUpdateRequest(BaseModel):
-    email: EmailStr
-
-
-class UserResendActivation(BaseModel):
-    email: EmailStr
-
-
-class ResetPass(BaseModel):
-    email: EmailStr
-
-
-class TourInfo(BaseModel):
-    tour_name: str
-
-
-class UserNewPassWord(BaseModel):
-    password: str
+class ClarifyProject(BaseModel):
+    chat_history: str
--- Backend/Routers/Projects/ProjectsResponseSchemas.py
+++ Backend/Routers/Projects/ProjectsResponseSchemas.py
@@ -3,13 +3,13 @@ from typing import Optional, List, Tuple, Union
 from datetime import datetime
 
 
-class UserRegistrationInfoResponse(BaseModel):
+class Project(BaseModel):
     """
     This is the schema for user login to send specific information, Not all.
     """
+    project_id: int
+    user_id: int
     name: str
-    family: str
-    email: str
 
     class Config:
         """
@@ -18,15 +18,9 @@ class UserRegistrationInfoResponse(BaseModel):
         from_attributes = True
 
 
-class UserLoginInfoResponse(BaseModel):
-    """
-    This is the schema for user login to send specific information, Not all.
-    """
-    name: str
-    family: str
-    email: str
-    country_iso: str
-    mobile: str
+class ClarifyProject(BaseModel):
+    query: str
+    chat_history: str
 
     class Config:
         """
@@ -34,35 +28,3 @@ class UserLoginInfoResponse(BaseModel):
         """
         from_attributes = True
 
-
-class User(BaseModel):
-    """
-    This is the schema for user login to send specific information, Not all.
-    """
-    user_id: int
-    name: str
-    family: str
-    email: str
-    country_iso: str
-    mobile: str
-    registration_time: datetime
-    enabled: bool
-
-    class config:
-        from_attributes = True
-        extra = Extra.allow
-
-
-
-class UserExistence(BaseModel):
-    user_id: int
-    name: Optional[str]
-    family: Optional[str]
-    email: Optional[str]
-    country_iso: Optional[str]
-    mobile: Optional[str]
-    registration_time: Optional[datetime]
-    enabled: Optional[bool]
-
-    class Config:
-        from_attributes = True
